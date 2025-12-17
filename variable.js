dd55158611e0b5653d02b8a05a8e37a69d411d9b
Ojiesuwa <oluwarotimiadeola@gmail.com>
2025-12-17 20:12:05 +0100

    Chaned the base URL

--- variable.js
+++ variable.js
@@ -1,1 +1,2 @@
-export const baseurl = "https://6447bb0c7e99.ngrok-free.app";
+// export const baseurl = "https://6447bb0c7e99.ngrok-free.app";
+export const baseurl = "https://saferesqhackathonbackend-1.onrender.com/";
